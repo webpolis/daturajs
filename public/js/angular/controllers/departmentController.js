90bc3d29cbc28f85adcb3be33122c0b4f105b319
Nicolás Iglesias <nico@webpolis.com.ar>
2013-02-13 18:14:55 -0300

    re #36, re #25, create gridService, new tag for data listing, save department

--- public/js/angular/controllers/departmentController.js
+++ public/js/angular/controllers/departmentController.js
@@ -1,37 +1,26 @@
-function departmentController($scope, $window, resourceService){
-    $scope.clientId = null, $scope.departments = [];
-    $scope.gridOptions = {
-        data: 'departments',
-        jqueryUITheme: true,
-        enableCellSelection: true,
-        displaySelectionCheckbox:false,
-        enablePaging: true,
-        pagingOptions:{
-            pageSizes: [25, 50, 100],
-            pageSize: 25,
-            totalServerItems: 0,
-            currentPage: 1
-        },
-        columnDefs:[
-            {
-                field:'id',
-                displayName:'ID',
-                maxWidth:50
-            },
-            {
-                field:'department_name',
-                displayName:'Name'
-            },
-            {
-                field:'dept_auth_limit',
-                displayName:'Authorization Limit'
-            },
-            {
-                field:'is_auth_required',
-                displayName:'Additional Authorization Required'
-            }
-        ]
-    };
+function departmentController($scope, $window, resourceService, gridService){
+    $scope.clientId = null, $scope.departments = [], $scope.department = {};
+    
+    // initialize data grid
+    var cols = [
+    {
+        name:'id',
+        label:'ID'
+    },
+    {
+        name:'department_name',
+        label:'Name'
+    },
+    {
+        name:'dept_auth_limit',
+        label:'Authorization Limit'
+    },
+    {
+        name:'is_auth_required',
+        label:'Additional Authorization'
+    }
+    ];
+    gridService($scope).init('departments', 25, cols);
 
     $scope.getDepartments = function(){
         resourceService.getDepartments({
@@ -43,6 +32,22 @@ function departmentController($scope, $window, resourceService){
             throw err
         })
     }
+    
+    $scope.departmentSave = function(){
+        $scope.department.client_id = $scope.clientId;
+
+        resourceService.departmentSave({
+            department: $scope.department
+        },function(ret){
+            if(ret.department){
+                $scope.department = ret.department
+                $scope.getDepartments()
+                $('#departmentForm').modal('hide')
+            }
+        },function(err){
+            throw err
+        })
+    }
 }
 
-departmentController.$inject = ['$scope', '$window', 'resourceService'];
+departmentController.$inject = ['$scope', '$window', 'resourceService', 'gridService'];
--- src/controllers/rest/departmentController.js
+++ src/controllers/rest/departmentController.js
@@ -1,7 +1,7 @@
+// @todo REST must have token authorization and some sort of mechanism to authenticate client
 module.exports = function(){
     var departmentController = {
         getDepartments : function (req,res, nxt){
-            // @todo REST must have token authorization and some sort of mechanism to authenticate client
             if(req.params.clientId){
                 var department = this.$$.models.department;
                 
@@ -19,6 +19,25 @@ module.exports = function(){
                 })
             }else
                 res.send(400,false);
+        },
+        departmentSave : function(req,res, nxt){
+            var department = req.body.department;
+            var ret = function(err){
+                res.send(err?400:200,{
+                    department: department
+                })
+            }
+            
+            if(department.id && department.id !== null){
+                
+            }else{
+                this.$$.models.department.$create(department, function(_department){
+                    department = _department;
+                    ret(false)
+                }, function(){
+                    ret(true)
+                })
+            }
         }
     };
     
